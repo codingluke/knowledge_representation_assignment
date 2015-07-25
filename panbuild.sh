2054b72762d9bb9c028fb1c50bb643fdbb2a1022
Roechi <msg.remus@gmail.com>
2015-07-25 22:20:26 +0200

    Changed font size.

--- panbuild.sh
+++ panbuild.sh
@@ -1,1 +1,1 @@
-pandoc --toc -s -N -V geometry:margin=1in --include-in-header titlesec.tex -o doku_bericht.pdf doku_bericht.md
+pandoc --toc -s -N -V fontsize=12pt -V geometry:margin=1in  --include-in-header titlesec.tex -o doku_bericht.pdf doku_bericht.md
