a3cd847de2c0e7871e6077a2f726c560cf82617f
Roechi <msg.remus@gmail.com>
2015-08-02 20:34:17 +0200

    Added bibliography commands.

--- panbuild.sh
+++ panbuild.sh
@@ -1,1 +1,3 @@
-pandoc --toc -s -N -V fontsize=12pt -V geometry:margin=1in  --include-in-header titlesec.tex -o bericht_hodel_remus.pdf bericht_hodel_remus.md
+#pandoc --toc -s -N -V fontsize=12pt -V geometry:margin=1in --filter pandoc-citeproc --biblio bericht.bib --csl chicago-author-date.csl --include-in-header titlesec.tex -o bericht_hodel_remus.pdf bericht_hodel_remus.md
+
+pandoc --filter pandoc-citeproc --toc -s -N -V fontsize=12pt -V geometry:margin=1in bericht_hodel_remus.md --biblio bericht.bib --include-in-header titlesec.tex --csl springer-lecture-notes-in-computer-science_modified.csl -o bericht_hodel_remus.pdf
